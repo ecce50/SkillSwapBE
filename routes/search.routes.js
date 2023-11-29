9b80b502ae9dd69e49a092239876190623ef99f1
Peter R <prodger2@gmail.com>
2023-11-29 10:23:24 +0100

    search functionality in place

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -4,24 +4,17 @@ const Class = require("../models/Class.model");
 
 router.get("/", async (req, res) => {
   const query = req.query.title;
-  const filter = req.query.filter;
 
   try {
     // Search both databases and retrieve results
-    const skillResults = await Skill.find({ title: new RegExp(query, "i") });
-    const classResults = await Class.find({ title: new RegExp(query, "i") });
+console.log("Search query:", query);
+const skillResults = await Skill.find({ title: new RegExp(query, "i") });
+const classResults = await Class.find({ title: new RegExp(query, "i") });
+console.log("Skill results:", skillResults);
+console.log("Class results:", classResults);
 
-    // Combine and filter the results based on the provided filter
-    let results = [];
-
-    if (filter === "skill") {
-      results = skillResults;
-    } else if (filter === "class") {
-      results = classResults;
-    } else {
-      // Default: Search both databases and combine the results
-      results = [...skillResults, ...classResults];
-    }
+    // Combine the results
+    const results = [...skillResults, ...classResults];
 
     res.json(results);
   } catch (error) {
