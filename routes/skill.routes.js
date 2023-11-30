79540db967e44973f9757e20eab44597c6d090c7
Peter R <prodger2@gmail.com>
2023-11-30 12:23:42 +0100

    skill crud is done

--- routes/skill.routes.js
+++ routes/skill.routes.js
@@ -4,7 +4,7 @@ const { isAuthenticated } = require("../middlewares/jwt.middleware");
 
 /*--------------------------------------- POST route to skill creation --------------------------------------------*/
 
-router.post("/skill-creation", isAuthenticated, async (req, res) => {
+router.post("/create-skill", isAuthenticated, async (req, res) => {
   const payload = req.body; // { email: 'someEmail', password '1234'}
 
   try {
@@ -21,6 +21,55 @@ router.post("/skill-creation", isAuthenticated, async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+router.put("/update-skill/:id", isAuthenticated, async (req, res) => {
+  const { userId } = req.user;
+  const { id } = req.params; // Extract skillId from the request parameters
+  const { title, description } = req.body;
+
+  try {
+    const skill = await Skill.findOne({ _id: id, teacherId: userId });
+
+    if (!skill) {
+      return res.status(404).json({ message: "Skill not found" });
+    }
+
+    // Update the skill with the new title and description
+    skill.title = title;
+    skill.description = description;
+
+    // Save the updated skill
+    await skill.save();
+
+    res.status(200).json({ message: "Skill updated successfully", skill });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// Delete skill route
+router.delete("/delete-skill/:id", isAuthenticated, async (req, res) => {
+  const { userId } = req.user;
+  const { id } = req.params; // Extract skillId from the request parameters
+
+  try {
+    const skill = await Skill.findOne({ _id: id});
+
+    if (!skill) {
+      return res.status(404).json({ message: "Skill not found" });
+    }
+
+    // Delete the skill
+    await skill.deleteOne();
+
+    res.status(200).json({ message: "Skill deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
 
 /*------------------------------------------ GET route to fetch skills by teacherId -------------------------------------*/
 router.get("/skills", isAuthenticated, async (req, res) => {
