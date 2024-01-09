064d48404a1904319ca42fef6113b2679bcae751
Peter R <prodger2@gmail.com>
2024-01-09 13:23:40 +0100

    profile crud

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -27,4 +27,54 @@ router.put("/add-skill", authenticateUser, async (req, res) => {
   }
 });
 
+// Update user profile
+router.patch("/update", authenticateUser, async (req, res) => {
+  const userId = req.body.userId;
+  const updatedFields = req.body;
+
+  try {
+    console.log("User ID in the try of the update route ", userId);
+        console.log("Updated fields in the try of the update route ", updatedFields);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { $set: updatedFields },
+      { new: true }
+    );
+
+    if (updatedUser) {
+      res.status(200).json({ message: "User profile updated successfully", user: updatedUser });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+});
+
+router.put("/update-profile", authenticateUser, async (req, res) => {
+  const { userId } = req.user;
+  const { email, userImage } = req.body;
+
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: userId },
+      { email, userImage },
+      { new: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "User profile updated successfully", user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
 module.exports = router;
